75ef097f3b513656eee50ec9eda665afc6214586
ElakkiyaNatarajan <30309298+ElakkiyaNatarajan@users.noreply.github.com>
2017-07-20 11:22:10 +0530

    Create cons.c

---
+++ cons.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+char cha;
+printf("Enter the  character:");
+scanf("%c",&cha);
+if(cha=='a'||cha=='e'||cha=='i'||cha=='o'||cha=='u'){
+printf("The character is  a vowel");}
+else{
+printf("The character is a consonant");}
+}
